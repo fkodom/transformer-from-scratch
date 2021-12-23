86076d22ca3a59d96811ff4965bdb75003fc7ff2
Frank Odom <frank.odom.iii@gmail.com>
2021-12-23 12:34:13 -0600

    Fix variable names in multi-head attention.

--- src/multi_head_attention.py
+++ src/multi_head_attention.py
@@ -22,12 +22,12 @@ class AttentionHead(nn.Module):
 
 
 class MultiHeadAttention(nn.Module):
-    def __init__(self, num_heads: int, dim_in: int, dim_k: int, dim_v: int):
+    def __init__(self, num_heads: int, dim_in: int, dim_q: int, dim_k: int):
         super().__init__()
         self.heads = nn.ModuleList(
-            [AttentionHead(dim_in, dim_k, dim_v) for _ in range(num_heads)]
+            [AttentionHead(dim_in, dim_q, dim_k) for _ in range(num_heads)]
         )
-        self.linear = nn.Linear(num_heads * dim_v, dim_in)
+        self.linear = nn.Linear(num_heads * dim_k, dim_in)
 
     def forward(self, query: Tensor, key: Tensor, value: Tensor) -> Tensor:
         return self.linear(
